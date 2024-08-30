62a70fdfceb609f34b5c1944e6690177ba5614fa
Abel Sekibaala <54054280+asekibaala@users.noreply.github.com>
2024-08-30 15:08:07 +0300

    Combinations and permutations
    For calculating probability

--- Ds_bed.R
+++ Ds_bed.R
@@ -4965,3 +4965,60 @@ events <- sample(beads, B, replace = TRUE)
 prop.table(table(events))
 
 x <- sample(beads, 5)
+
+
+#Combinations and permutations
+number <- "Three"
+suit <- "Hearts"
+paste(number, suit)
+
+paste(letters[1:5], as.character(1:5))
+
+expand.grid(pants = c("blue", "black"), shirt = c("white", "grey", "plaid"))
+
+#Here is how we generate a deck of cards:
+suits <- c("Diamonds", "Clubs", "Hearts", "Spades")
+numbers <- c("Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", 
+             "Eight", "Nine", "Ten", "Jack", "Queen", "King")
+deck <- expand.grid(number = numbers, suit = suits)
+deck <- paste(deck$number, deck$suit)
+
+kings <- paste("King", suits)
+mean(deck %in% kings)
+
+library(gtools)
+permutations(3, 2)
+
+all_phone_numbers <- permutations(10, 7, v = 0:9)
+n <- nrow(all_phone_numbers)
+index <- sample(n, 5)
+all_phone_numbers[index,]
+
+
+hands <- permutations(52, 2, v = deck)
+
+first_card <- hands[,1]
+second_card <- hands[,2]
+
+kings <- paste("King", suits)
+sum(first_card %in% kings)
+
+sum(first_card %in% kings & second_card %in% kings) / 
+  sum(first_card %in% kings)
+
+
+#if order doesnot matter use combinations not permutations 
+
+combinations(3,2)
+
+aces <- paste("Ace", suits)
+
+facecard <- c("King", "Queen", "Jack", "Ten")
+facecard <- expand.grid(number = facecard, suit = suits)
+facecard <- paste(facecard$number, facecard$suit)
+
+hands <- combinations(52, 2, v = deck)
+
+mean(hands[,1] %in% aces & hands[,2] %in% facecard)
+
+#Monte Carlo
